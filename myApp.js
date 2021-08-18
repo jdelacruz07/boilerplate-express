4d5a33b1b875dd1333551fbdb62058a1ab34d940
Julio de la Cruz <jdelacruz94@msn.com>
2021-08-18 09:41:04 +0200

    Add get request with query

--- myApp.js
+++ myApp.js
@@ -23,6 +23,12 @@ app.post("/name", (req, res) => {
     res.json({ name: `${firstName} ${lastName}` })
 });
 
+app.get("/name", (req, res) => {
+    var firstName = req.query.first;
+    var lastName = req.query.last;
+    res.json({ name: `${firstName} ${lastName}` })
+});
+
 app.get("/:word/echo", (req, res) =>
     // res.send({"echo": req.params.word})
     res.json(req.params)
